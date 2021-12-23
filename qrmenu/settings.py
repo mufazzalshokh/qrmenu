d4e697535f2111161624032172dcdc19bea28b85
Zafar Berdinazarov <zafarberdinazarov@Zafars-MacBook-Air.local>
2021-12-23 14:41:58 +0500

    psql

--- qrmenu/settings.py
+++ qrmenu/settings.py
@@ -58,23 +58,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'qrmenu.wsgi.application'
 
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 		'NAME': config('DB_NAME'),
-# 		'USER': config('DB_USER'),
-# 		'PASSWORD': config('DB_PASS'),
-# 		'HOST': config('DB_HOST'),
-# 		'PORT': config('DB_PORT'),
-# 	}
-# }
-
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': config('DB_NAME'),
+		'USER': config('DB_USER'),
+		'PASSWORD': config('DB_PASS'),
+		'HOST': config('DB_HOST'),
+		'PORT': config('DB_PORT'),
 	}
 }
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': BASE_DIR / 'db.sqlite3',
+# 	}
+# }
 AUTH_PASSWORD_VALIDATORS = [
 	{
 		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
